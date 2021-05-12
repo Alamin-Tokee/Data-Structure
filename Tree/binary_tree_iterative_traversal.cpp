af98f687013a645ec62fc019b10767f7b2e6942b
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-12 22:29:32 +0600

    Iterative post order traversal add

--- Tree/binary_tree_iterative_traversal.cpp
+++ Tree/binary_tree_iterative_traversal.cpp
@@ -17,3 +17,25 @@ void iterativePreorder(Node* root){
 	}
 }
 
+void iterativePostorder(Node* root){
+	if(root==NULL) return;
+
+	stack<Node*>st,stt;
+	st.push(root);
+	while(!st.empty()){
+		Node* node=st.top();
+		stt.push(node);
+		st.pop();
+
+		if(node->left){
+			st.push(node->left);
+		}
+		if(node->right){
+			st.push(node->right);
+		}
+	}
+	while(!stt.empty()){
+		cout<<stt.top()<< '\n';
+		stt.pop();
+	}
+}
