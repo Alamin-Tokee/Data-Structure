22c39c8273db8233831f58a417f27891adbb524c
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-26 23:56:27 +0600

    Create linked list update

--- Linked_List/create_linked_list.cpp
+++ Linked_List/create_linked_list.cpp
@@ -1,3 +1,5 @@
+
+//Using C 
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -29,4 +31,45 @@ int main(){
 	printf("%d\n", node->data);
 
 	return 0;
+}
+
+
+//Using CPP
+#include <bits/stdc++.h>
+using namespace std;
+
+// Creating a node
+class Node {
+  public:
+    int value;
+    Node* next;
+};
+
+int main() {
+     Node* head;
+     Node* one = NULL;
+     Node* two = NULL;
+     Node* three = NULL;
+
+     // allocate 3 nodes in the heap
+     one = new Node();
+     two = new Node();
+     three = new Node();
+
+  
+     one->value = 1;
+     two->value = 2;
+     three->value = 3;
+
+     one->next = two;
+     two->next = three;
+     three->next = NULL;
+
+     head = one;
+     while (head != NULL) {
+        printf("%d ", head->value);
+        head = head->next;
+     }
+   
+     return 0;
 }
