5dfa29d24a30c0b4672e072924bf1b5fbe414845
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-28 23:12:01 +0600

    Count linked list value

---
+++ Linked_List/count_linked_list.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+typedef struct node Node;
+
+struct node{
+	int data;
+	Node* next;
+};
+
+Node* CreateNode(int item,Node* next){
+	Node* newnode=(Node*)malloc(Node);
+	if(newnode==NULL){
+		std::cout<< "Error"<<std::endl;
+		exit(1);
+	}
+	newnode->data=item;
+	newnode->next=next;
+}
+
+Node* append(int item,Node* head){
+	Node* newnode=CreateNode(item,NULL);
+
+	if(head==NULL) return newnode;
+
+	Node* currentNode=head;
+
+	while(currentNode->next!=NULL){
+		currentNode=currentNode->next;
+	}
+	currentNode->next=newnode;
+
+	return head;
+}
+
+int countLinkedList(Node* head){
+	Node* current=head;
+	int count=0;
+	while(current->next!=NULL){
+		current=current->next;
+		count++;;
+	}
+	return count;
+}
+
+int main(){
+	Node* head, *n1, *n2;
+
+	n1=CreateNode(10,NULL);
+
+	head=n1;
+	std::cout<< head->data <<std::endl;
+
+	head=append(12,head);
+
+	n2=head->next;
+
+	std::cout<< n2->data << std::endl;
+
+	int val=countLinkedList(head);
+
+	std::cout<< val << std::endl;
+
+
+	return 0;
+}
