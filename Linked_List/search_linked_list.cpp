8d3a3df04edb0b3f93cb8976c21c9ecaaba6bec0
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-28 23:19:30 +0600

    Search element from linked list

---
+++ Linked_List/search_linked_list.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+typedef struct node Node;
+
+struct node{
+	int data;
+	Node* next;
+};
+
+Node* CreateNode(int item,Node* next){
+	Node* newnode=(Node*)malloc(Node);
+	if(newnode==NULL){
+		std::cout<< "Error"<<std::endl;
+		exit(1);
+	}
+	newnode->data=item;
+	newnode->next=next;
+}
+
+Node* append(int item,Node* head){
+	Node* newnode=CreateNode(item,NULL);
+
+	if(head==NULL) return newnode;
+
+	Node* currentNode=head;
+
+	while(currentNode->next!=NULL){
+		currentNode=currentNode->next;
+	}
+	currentNode->next=newnode;
+
+	return head;
+}
+
+Node* searchLinkedList(int item,Node* head){
+	Node* current=head;
+	if(head==NULL) return nullptr;
+	
+	while(current->next!==NULL){
+		if(current->data==item){
+			return current;
+			break;
+		}
+		current=current->next;
+	}
+
+	return nullptr;
+}
+
+int main(){
+	Node* head, *n1, *n2;
+
+	n1=CreateNode(10,NULL);
+
+	head=n1;
+	std::cout<< head->data <<std::endl;
+
+	head=append(12,head);
+
+	n2=head->next;
+
+	std::cout<< n2->data << std::endl;
+
+	Node* node=searchLinkedList(10,head);
+
+	std::cout << node->data << std::endl;
+
+
+	return 0;
+}
